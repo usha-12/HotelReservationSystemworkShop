54559aedab29a8db0df3ffb5491dcd1f0803f065
usha-12 <usha12ahirwar@gmail.com>
2022-11-15 17:06:03 +0530

    Add hotels

--- src/brideglabz/com/HotelReservationSystem/HotelData.java
+++ src/brideglabz/com/HotelReservationSystem/HotelData.java
@@ -1,2 +1,57 @@
-package brideglabz.com.HotelReservationSystem;public class HotelData {
+package brideglabz.com.HotelReservationSystem;
+
+public class HotelData {
+    String hotelName;
+    int weekdayRates;
+    int weekendRates;
+    int totalPrice;
+    int hotelRating;
+
+    public HotelData(String hotelName, int weekdayRates, int weekendRates, int totalPrice, int hotelRating) {
+        this.hotelName = hotelName;
+        this.weekdayRates = weekdayRates;
+        this.weekendRates = weekendRates;
+        this.totalPrice = totalPrice;
+        this.hotelRating = hotelRating;
+    }
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public int getWeekdayRates() {
+        return weekdayRates;
+    }
+
+    public void setWeekdayRates(int weekdayRates) {
+        this.weekdayRates = weekdayRates;
+    }
+
+    public int getWeekendRates() {
+        return weekendRates;
+    }
+
+    public void setWeekendRates(int weekendRates) {
+        this.weekendRates = weekendRates;
+    }
+
+    public int getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(int totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public int getHotelRating() {
+        return hotelRating;
+    }
+
+    public void setHotelRating(int hotelRating) {
+        this.hotelRating = hotelRating;
+    }
 }
--- src/brideglabz/com/HotelReservationSystem/HotelReservationSystem.java
+++ src/brideglabz/com/HotelReservationSystem/HotelReservationSystem.java
@@ -1,2 +1,29 @@
-package brideglabz.com.HotelReservationSystem;public class HotelReservationSystem {
+package brideglabz.com.HotelReservationSystem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HotelReservationSystem {
+    public static void main(String[] args) {
+        System.out.println("----------Welcome to the Hotel reservation System program---------");
+        List<Hotel> hotelDataList = new ArrayList<>();
+        Hotel hotel1 = new Hotel("LakeWood",110,90,80,80);
+        Hotel hotel2 = new Hotel("Bridgewood",160,110,60,50);
+        Hotel hotel3 = new Hotel("Ridgewood",220, 100,150, 40);
+        hotelDataList.add(hotel1);
+        hotelDataList.add(hotel2);
+        hotelDataList.add(hotel3);
+        System.out.println(hotelDataList);
+
+    }
 }
+/*Ability to add Hotel in a Hotel
+Reservation System with Name and
+rates for Regular Customer...
+- Program is written using IDE like IntelliJ/Eclipse/VS Code
+- Every UC is in a separate Git Branch and then merged with main
+- Every UC should have a corresponding Test Case. Here Employees should
+be added to payroll system using a Test Case
+- Naming Convention, Indentation, etc Code Hygiene will be checked during
+Review
+- Git Check In Comments and Version History will be monitored*/
